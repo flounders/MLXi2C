99c13064b897ba413a1686f6a32db2a4c874a856
Steven Williams <steven.m.williams@yandex.com>
2016-07-25 18:16:00 -0400

    Cleaned up the initial port of the C code. Now I can average several samples taken over a short period of time leading to a more accurate reading.

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,49 @@
 module Main where
 
+import Control.Concurrent
+import Control.Monad
 import qualified Data.ByteString as B
-import System.RaspberryPi.GPIO 
 import Data.Word
+import System.RaspberryPi.GPIO 
 
 main :: IO ()
-main = withGPIO . withI2C $ readTemp
+main = 
+  do temps <- withGPIO . withI2C $ replicateM sampleRate readTemp
+     let (aTemps, oTemps) = unzip temps
+     putStr $ "Ambient temp: " ++ show (avgTemp sampleRate aTemps) ++ " C; "
+     putStrLn $ "Object temp: " ++ show (avgTemp sampleRate oTemps) ++ " C"
+  where sampleRate = 10
 
-readTemp :: IO ()
+-- readTemp :: Fractional a => IO (a, a)
 readTemp =
   do setI2cBaudRate 25000
-     aTemp <- ambientTemp
-     iTemp <- infraTemp
-     putStrLn $ "Ambient temperature: " ++ show aTemp ++ "C"
-     putStrLn $ "Infrared temperature: " ++ show iTemp ++ "C"
+     getTemp
+  where getTemp = do aTemp <- ambientTemp
+                     oTemp <- objectTemp
+                     return (aTemp, oTemp)
 
-infraTemp :: Fractional a => IO a
-infraTemp = 
-  do bytes <- writeReadRSI2C 0x5a (B.singleton 7) 2
-     return (bytesToKelvin bytes - 273.15)
+objectTemp :: Fractional a => IO a
+objectTemp = genericTemp 7 
 
-ambientTemp = 
-  do bytes <- writeReadRSI2C 0x5a (B.singleton 6) 2
-     return (bytesToKelvin bytes - 273.15)
+ambientTemp :: Fractional a => IO a
+ambientTemp = genericTemp 6 
+
+genericTemp :: Fractional a => Word8 -> IO a
+genericTemp sen =
+  do bytes <- readMLX sen
+     return . kelvinToCelsius . bytesToKelvin $ bytes
+
+avgTemp :: Fractional a => Int -> [a] -> a
+avgTemp n temps = sum temps / fromIntegral n
+
+readMLX :: Word8 -> IO B.ByteString
+readMLX s = return . B.take 2 =<< writeReadRSI2C 0x5a (B.singleton s) 3
 
 bytesToInteger :: [Word8] -> Integer
 bytesToInteger = foldl (\acc x -> fromIntegral x + acc * 16^2) 0
 
 bytesToKelvin :: Fractional a => B.ByteString -> a
 bytesToKelvin = (/50) . fromIntegral . bytesToInteger . reverse . B.unpack
+
+kelvinToCelsius :: Fractional a => a -> a
+kelvinToCelsius temp = temp - 273.15
