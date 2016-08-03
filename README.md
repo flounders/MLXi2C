ec7f584098ae10cddba097051b3e44d0d1c5c90f
Steven Williams <steven.m.williams@yandex.com>
2016-08-03 05:33:41 -0400

    Added some error handling for reading from the sensor so if the mst significant bit is set that reading is ignored.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This is just some sample code for using the HPi module with the Melexis MLX90614 contact-less
 temperature sensor. You will need the [C library for BCM 2835](http://www.airspayce.com/mikem/bcm2835/)
 to build the code.
+
+To build this sample run the following:
+
+```
+$ cabal sandbox init
+$ cabal build
+# ./dist/build/MLXi2C/MLXi2C
+```
+
+There may be a way to run the program without root, but I haven't found it yet.
--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Concurrent
 import Control.Monad
 import qualified Data.ByteString as B
+import Data.Maybe (isJust, fromJust)
 import Data.Word
 import System.RaspberryPi.GPIO 
 
@@ -10,8 +11,8 @@ main :: IO ()
 main = 
   do temps <- withGPIO . withI2C $ replicateM sampleRate readTemp
      let (aTemps, oTemps) = unzip temps
-     putStr $ "Ambient temp: " ++ show (avgTemp sampleRate aTemps) ++ " C; "
-     putStrLn $ "Object temp: " ++ show (avgTemp sampleRate oTemps) ++ " C"
+     putStr $ "Ambient temp: " ++ show (avgTemp . map fromJust . filter isJust $ aTemps) ++ " C; "
+     putStrLn $ "Object temp: " ++ show (avgTemp . map fromJust . filter isJust $ oTemps) ++ " C"
   where sampleRate = 10
 
 -- readTemp :: Fractional a => IO (a, a)
@@ -22,19 +23,21 @@ readTemp =
                      oTemp <- objectTemp
                      return (aTemp, oTemp)
 
-objectTemp :: Fractional a => IO a
+objectTemp :: Fractional a => IO (Maybe a)
 objectTemp = genericTemp 7 
 
-ambientTemp :: Fractional a => IO a
+ambientTemp :: Fractional a => IO (Maybe a)
 ambientTemp = genericTemp 6 
 
-genericTemp :: Fractional a => Word8 -> IO a
+genericTemp :: Fractional a => Word8 -> IO (Maybe a)
 genericTemp sen =
   do bytes <- readMLX sen
-     return . kelvinToCelsius . bytesToKelvin $ bytes
+     return $ kelvinToCelsius <$> bytesToKelvin bytes
 
-avgTemp :: Fractional a => Int -> [a] -> a
-avgTemp n temps = sum temps / fromIntegral n
+avgTemp :: Fractional a => [a] -> a
+avgTemp = g . foldr f (0,0) 
+  where f x (total, length) = (x + total, length + 1)
+        g (total, length) = total / length
 
 readMLX :: Word8 -> IO B.ByteString
 readMLX s = return . B.take 2 =<< writeReadRSI2C 0x5a (B.singleton s) 3
@@ -42,8 +45,14 @@ readMLX s = return . B.take 2 =<< writeReadRSI2C 0x5a (B.singleton s) 3
 bytesToInteger :: [Word8] -> Integer
 bytesToInteger = foldl (\acc x -> fromIntegral x + acc * 16^2) 0
 
-bytesToKelvin :: Fractional a => B.ByteString -> a
-bytesToKelvin = (/50) . fromIntegral . bytesToInteger . reverse . B.unpack
+bytesToKelvin :: Fractional a => B.ByteString -> Maybe a
+bytesToKelvin bs = (/50) . fromIntegral . bytesToInteger <$> (validBytes . reverse . B.unpack $ bs)
 
 kelvinToCelsius :: Fractional a => a -> a
 kelvinToCelsius temp = temp - 273.15
+
+validBytes :: [Word8] -> Maybe [Word8]
+validBytes (x:xs)
+  | x <= 128 = Just (x:xs)
+  | otherwise = Nothing
+validBytes _ = Nothing
